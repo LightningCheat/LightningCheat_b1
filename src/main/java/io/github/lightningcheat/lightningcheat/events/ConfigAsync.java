0794af9a9d80a6135c88f8ad0c67c2b6e99a7d89
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2020-10-07 17:25:49 +0800

    Update ConfigAsync.java

--- src/main/java/io/github/lightningcheat/lightningcheat/events/ConfigAsync.java
+++ src/main/java/io/github/lightningcheat/lightningcheat/events/ConfigAsync.java
@@ -1,4 +1,18 @@
 package io.github.lightningcheat.lightningcheat.events;
 
+import io.github.lightningcheat.lightningcheat.Lightningcheat;
+import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber(modid = Lightningcheat.MOD_ID)
 public class ConfigAsync {
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(Lightningcheat.MOD_ID)) {
+            ConfigManager.sync(Lightningcheat.MOD_ID, Config.Type.INSTANCE);
+        }
+    }
 }
